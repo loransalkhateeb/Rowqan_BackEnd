41551f3c9efdcb76853f3d633285a5b8e00f76f6
lorans alkhateeb <lwrnsalkhtyb9@gmail.com>
2024-12-02 01:26:11 +0300

    Adding Cloudinary

--- Config/Multer.js
+++ Config/Multer.js
@@ -1,41 +1,15 @@
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+const cloudinary = require('../Config/cloudinaryConfig');
 
-const customStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = "uploads/";
-
-    
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    const filename = file.originalname;
-    const filePath = path.join("uploads", filename);
-
-    if (fs.existsSync(filePath)) {
-      const timestamp = Date.now();
-      const ext = path.extname(filename);
-      const baseName = path.basename(filename, ext);
-      cb(null, `${baseName}-${timestamp}${ext}`);
-    } else {
-      cb(null, filename);
-    }
-  },
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'uploads/chalets_images', 
+        allowed_formats: ['jpg', 'png', 'mp4'], 
+    },
 });
 
-
-const fileFilter = (req, file, cb) => {
-  cb(null, true);
-};
-
-const upload = multer({
-  storage: customStorage,
-  fileFilter, 
-  limits: { fileSize: 1024 * 1024 * 10 }, 
-});
+const upload = multer({ storage });
 
 module.exports = upload;
