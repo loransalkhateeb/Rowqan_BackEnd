4b1a8fe4c7f06fab6708cd3ce1181d7a9c2be06f
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-12-14 14:40:17 +0300

    Update

--- Routes/ServicesRoutes.js
+++ Routes/ServicesRoutes.js
@@ -5,7 +5,7 @@ const multer = require('../Config/Multer');
 const authMiddleware = require('../MiddleWares/authMiddleware'); 
 const rateLimiter = require('../MiddleWares/rateLimiter');
 
-router.use(authMiddleware);
+
 
 router.post('/createService', rateLimiter, multer.single('image'), servicesController.createService);
 
@@ -13,11 +13,15 @@ router.get('/getAllServices/:lang', servicesController.getAllServices);
 
 router.get('/getAllServicesByServiceStatus/:status_service/:lang', servicesController.getServiceByStatus);
 
+
 router.get('/getAllServicesByServiceStatus/:lang', servicesController.getServiceByStatusOnlyLang);
+
 
 router.get('/getservicebyid/:id/:lang', servicesController.getServiceById);
 
+
 router.put('/updateService/:id', rateLimiter, multer.single('image'), servicesController.updateService);
+
 
 router.delete('/deleteService/:id/:lang', servicesController.deleteService);
 
