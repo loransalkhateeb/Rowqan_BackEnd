76d17d2f68e6574b61c248858aab7f0786e8ae87
loransalkhateeb <lwrnsalkhtyb9@gmail.com>
2024-12-14 14:52:18 +0300

    Updated On Hero

--- Routes/HeroRoutes.js
+++ Routes/HeroRoutes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const heroController = require('../Controllers/HeroController');  
-const multer = require('../Config/Multer'); 
-const authMiddleware = require('../MiddleWares/authMiddleware');  
-const rateLimiter = require('../MiddleWares/rateLimiter'); 
+const heroController = require('../Controllers/HeroController');
+const multer = require('../Config/Multer');
+const authMiddleware = require('../MiddleWares/authMiddleware');
+const rateLimiter = require('../MiddleWares/rateLimiter');
 
 
 router.post('/createHero', authMiddleware, rateLimiter, multer.single('image'), heroController.createHero);
 
-router.get('/getAllHeroes/:lang', heroController.getAllHeroesByLang);
+
+router.get('/getAllHeroes/:lang', heroController.getHeroesByLang);
+
 
 router.get('/getHeroById/:id/:lang', heroController.getHeroById);
 
 router.put('/updateHero/:id', authMiddleware, rateLimiter, multer.single('image'), heroController.updateHero);
+
 
 router.delete('/deleteHero/:id/:lang', authMiddleware, heroController.deleteHero);
 
